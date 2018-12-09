fc928cfa24c4fd5179c050ca6b7f61856ffd5384
John McGuigan <jrm5100@gmail.com>
2018-12-09 12:10:24 -0500

    Prep Day 10

---
+++ 2018/10/solution.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+##############
+# Load input #
+##############
+
+
+with open("input.txt", "r") as f:
+    input_data = f.read().strip()
+
+
+##############
+# Solution 1 #
+##############
+
+answer = ""
+print(f"Solution to part 1 is {answer}")
+
+##############
+# Solution 2 #
+##############
+
+answer = ""
+print(f"Solution to part 2 is {answer}")
