349412aa966fed30a8985d56e2873fa2f62c3618
John McGuigan <jrm5100@gmail.com>
2018-12-10 20:19:24 -0500

    Finish Day 10 and prepare day 11

--- 2018/10/solution.py
+++ 2018/10/solution.py
@@ -1,24 +1,61 @@
 #!/usr/bin/python3
 
+# Note: this solution is more interactive, given the limited ways of calculating a precise mathematical solution
+
 ##############
 # Load input #
 ##############
 
-
+points = []
 with open("input.txt", "r") as f:
-    input_data = f.read().strip()
-
+    for line in f:
+        x = int(line.split('<')[1].split(',')[0])
+        y = -1 * int(line.split(',')[1].split('>')[0])
+        vx = int(line.split('<')[2].split(',')[0])
+        vy = -1 * int(line.split(',')[2].split('>')[0])
+        points.append(dict(x=x, y=y, vx=vx, vy=vy))
 
 ##############
 # Solution 1 #
 ##############
 
-answer = ""
-print(f"Solution to part 1 is {answer}")
+def update_points(points):
+    for p in points:
+        p["x"] += p["vx"]
+        p["y"] += p["vy"]
+    return points
 
-##############
-# Solution 2 #
-##############
+def print_grid(points):
+    # Get the range
+    xlist = [p["x"] for p in points]
+    ylist = [p["y"] for p in points]
+    xmin = min(xlist)
+    xmax = max(xlist)
+    ymin = min(ylist)
+    ymax = max(ylist)
 
-answer = ""
-print(f"Solution to part 2 is {answer}")
+    grid = []
+    for i in range(ymin, ymax+1):
+        xpoints = [p["x"] for p in points if p["y"] == i]
+        grid.append("".join(['.' if n not in xpoints else '#' for n in range(xmin, xmax+1)]))
+    # Must reverse to get the y-order back to the original orientation
+    for row in grid[::-1]:
+        print(row)
+
+unique_x = set([p["x"] for p in points])
+unique_y = set([p["y"] for p in points])
+
+passed_seconds = 0
+# Skip ahead until they are more lined up
+while len(unique_x) > 50 and len(unique_y) > 50:
+    points = update_points(points)
+    passed_seconds += 1
+    unique_x = set([p["x"] for p in points])
+    unique_y = set([p["y"] for p in points])
+
+while True:
+    print(f"----- {passed_seconds:,} seconds passed -----")
+    print_grid(points)
+    input()
+    points = update_points(points)
+    passed_seconds += 1
