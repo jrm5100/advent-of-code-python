3a0bc4c608bfa0cfa49ad0142ecb1e517087af90
John McGuigan <jrm5100@gmail.com>
2018-12-14 12:37:25 -0500

    Finish Day 14 with a simpler approach

--- 2018/14/solution.py
+++ 2018/14/solution.py
@@ -1,61 +1,53 @@
 #!/usr/bin/python3
 
-from typing import List
-
 ##############
 # Load input #
 ##############
 
-class Elf:
-    def __init__(self, idx, board):
-        self.idx = idx
-        self.board = board
-        self.score = self.board[self.idx]
-    
-    def move(self):
-        """Move the elf and return the next score"""
-        self.idx += self.score + 1
-        while self.idx >= len(board):
-            self.idx -= len(board)
-        self.score = self.board[self.idx]
-
 # Initialize data
-board = [3, 7]
-elf1 = Elf(0, board)
-elf2 = Elf(1, board)
+board = "37"
+elf1 = 0
+elf2 = 1
 
 ##############
 # Solution 1 #
 ##############
 
-def next_10_simple(board: List[int], elf1: Elf, elf2: Elf, prior_recipes: int) -> str:
+def next_10(board: str, elf1: int, elf2: int, prior_recipes: int) -> str:
     """Return the 10 scores appearing after the given number of scores"""
     while len(board) < prior_recipes + 10:
-        elf1.move()
-        elf2.move()
-        new_recipe = elf1.score + elf2.score
-        for c in str(new_recipe):
-            board.append(int(c))
-    return "".join(str(n) for n in board[-10:])
+        # Move elves
+        elf1 = (elf1 + int(board[elf1]) + 1) % len(board)
+        elf2 = (elf2 + int(board[elf2]) + 1) % len(board)
+        # Update scores
+        board += str(int(board[elf1]) + int(board[elf2]))
+    return board[-10:]
 
-answer = next_10_simple(board, elf1, elf2, 864801)
+answer = next_10(board, elf1, elf2, 864801)
 print(f"Solution to part 1 is {answer}")
 
 ##############
 # Solution 2 #
 ##############
 
-# Reset data to avoid checking the existing board, even though that would be more efficient
-board = [3, 7]
-elf1 = Elf(0, board)
-elf2 = Elf(1, board)
+# I was initially stuck because I didn't consider that the sequence might be added with
+# an extra digit after it, so the sequence might not appear at the very end of the board.
+# This made me waste time trying to optimize things, thinking the answer was > 500 million
 
-def number_to_left(board: List[int], elf1: Elf, elf2: Elf, sequence: int) -> int:
+board = "37"
+elf1 = 0
+elf2 = 1
+
+def number_to_left(board:str, elf1: int, elf2: int, sequence: str) -> int:
     """Return the number of scores appearing before the given sequence"""
-    sequence = [int(n) for n in str(sequence)]
-    # TODO
-    return len(board) - len(sequence)
+    while sequence not in board[-len(sequence)-1:]:
+        # Move elves
+        elf1 = (elf1 + int(board[elf1]) + 1) % len(board)
+        elf2 = (elf2 + int(board[elf2]) + 1) % len(board)
+        # Update scores
+        board += str(int(board[elf1]) + int(board[elf2]))
+    return board.find(sequence)
     
-answer = number_to_left(board, elf1, elf2, 864801)
+answer = number_to_left(board, elf1, elf2, "864801")
 
 print(f"Solution to part 2 is {answer}")
